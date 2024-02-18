3aa270a2750c037eb4e72c1434d835f94caf940b
MarianoVilla <dager_a7x@hotmail.com>
2024-02-18 19:17:59 -0300

    Send HTTP OK

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,17 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+Console.WriteLine("Init");
 
-// Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+using (Socket socket = server.AcceptSocket())
+{
+    var ResponseBuff =  Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+    socket.Send(ResponseBuff);
+}
+
+
+
+
